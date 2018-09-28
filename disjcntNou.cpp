7458ea82c0ac95527013bbc6c7a14e87f1a55849
Gavan Adrian <adi.gavan@gmail.com>
2018-09-28 14:44:40 +0300

    Update disjcntNou.cpp

--- disjcntNou.cpp
+++ disjcntNou.cpp
@@ -8,7 +8,7 @@ class Task {
  public:
 	void solve() {
 		read_input();
-    get_result();
+    		get_result();
 		print_output();
 	}
 
@@ -16,83 +16,82 @@ class Task {
 	int n;
 	int m;
 	vector<int> adj[kNmax];
-  map<pair<int, int>, int> frecvente;
-  int x[kNmax], y[kNmax], idx[kNmax], low[kNmax], dim_cmp_conexa[kNmax];
-  bool crit_edge[kNmax], visited[kNmax], visited2[kNmax];
-  int nr_comp_conexe;
-  long long numar_perechi = 0;
-
+	map<pair<int, int>, int> frecvente;
+	int x[kNmax], y[kNmax], idx[kNmax], low[kNmax], dim_cmp_conexa[kNmax];
+	bool crit_edge[kNmax], visited[kNmax], visited2[kNmax];
+	int nr_comp_conexe;
+	long long numar_perechi = 0;
 
 	void read_input() {
 		ifstream fin("disjcnt.in");
 		fin >> n >> m;
 
-    for (int i = 1; i <= m; i++) {
-  		fin >> x[i] >> y[i];
-  		adj[x[i]].push_back(i);
-  		adj[y[i]].push_back(i);
-  		frecvente[make_pair(x[i], y[i])]++;
-  	}
+		for (int i = 1; i <= m; i++) {
+			fin >> x[i] >> y[i];
+			adj[x[i]].push_back(i);
+			adj[y[i]].push_back(i);
+			frecvente[make_pair(x[i], y[i])]++;
+		}
 
 		fin.close();
 	}
 
-  // Functie muchii critice
-  void tarjanCritice(int node, int father) {
-  	idx[node] = idx[father] + 1;
-  	low[node] = idx[node];
-    visited[node] = true;
+	  // Functie muchii critice
+	void tarjanCritice(int node, int father) {
+		idx[node] = idx[father] + 1;
+		low[node] = idx[node];
+		visited[node] = true;
 
-  	for (int edge : adj[node]) {
-  		int v = x[edge] + y[edge] - node;
-      if (v == father) continue;
-  		if (!visited[v]) {
-  			tarjanCritice(v, node);
+		for (int edge : adj[node]) {
+			int v = x[edge] + y[edge] - node;
+			if (v == father) continue;
+			if (!visited[v]) {
+				tarjanCritice(v, node);
 
-  			low[node] = min(low[node], low[v]);
-  			if (low[v] > idx[node] && (frecvente[make_pair(node, v)] +
-                                        frecvente[make_pair(node, v)] <= 2)) {
-  				crit_edge[edge] = true;
-  			}
-  		} else {
-  			low[node] = min(low[node], idx[v]);
-  		}
-  	}
-  }
+				low[node] = min(low[node], low[v]);
+				if (low[v] > idx[node] && (frecvente[make_pair(node, v)] +
+						frecvente[make_pair(node, v)] <= 2)) {
+					crit_edge[edge] = true;
+				}
+			} else {
+				low[node] = min(low[node], idx[v]);
+			}
+		}
+	  }
 
-  // Functie pentru componente conexe
-  void componente_conexe(int node) {
-  	visited2[node] = true;
-  	dim_cmp_conexa[nr_comp_conexe]++;
+	  // Functie pentru componente conexe
+	  void componente_conexe(int node) {
+		visited2[node] = true;
+		dim_cmp_conexa[nr_comp_conexe]++;
 
-  	for (int edge : adj[node]) {
-  		int v = x[edge] + y[edge] - node;
-  		if (!visited2[v] && !crit_edge[edge]) {
-  			componente_conexe(v);
-  		}
-  	}
-  }
+		for (int edge : adj[node]) {
+			int v = x[edge] + y[edge] - node;
+			if (!visited2[v] && !crit_edge[edge]) {
+				componente_conexe(v);
+			}
+		}
+	  }
 
-	void get_result() {
-    tarjanCritice(1, 0);
+	  void get_result() {
+		tarjanCritice(1, 0);
 
-  	for (int i = 1; i <= n; i++) {
-  		if (!visited2[i]) {
-  			nr_comp_conexe++;
-  			componente_conexe(i);
-  		}
-  	}
+		for (int i = 1; i <= n; i++) {
+			if (!visited2[i]) {
+				nr_comp_conexe++;
+				componente_conexe(i);
+			}
+		}
 
-  	for (int i = 1; i <= nr_comp_conexe; i++) {
-  	   numar_perechi += (1LL * dim_cmp_conexa[i] * (dim_cmp_conexa[i] - 1)) / 2;
-    }
-	}
+		for (int i = 1; i <= nr_comp_conexe; i++) {
+		   numar_perechi += (1LL * dim_cmp_conexa[i] * (dim_cmp_conexa[i] - 1)) / 2;
+		}
+	   }
 
-	void print_output() {
+	   void print_output() {
 		ofstream fout("disjcnt.out");
 		fout << numar_perechi << '\n';
 		fout.close();
-	}
+	   }
 };
 
 int main() {
